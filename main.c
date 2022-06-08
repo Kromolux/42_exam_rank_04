eefde64675ddf27c386ba9057bd799c077cb94e0
rkaufman <rkaufman@student.42wolfsburg.de>
2022-06-08 21:37:33 +0200

    further improved

--- main.c
+++ main.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: rkaufman <rkaufman@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/08 18:25:18 by rkaufman          #+#    #+#             */
-/*   Updated: 2022/06/08 21:22:59 by rkaufman         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -19,20 +7,14 @@ int	ft_strlen(char *s)
 {
 	int i = 0;
 	while (s[i])
-	{
 		i++;
-	}
 	return (i);
 }
 
-void	ft_write_fd(char *s)
+void	ft_write(char *s1, char *s2)
 {
-	write(STDERR_FILENO, s, ft_strlen(s));
-}
-
-void	ft_write_fd_nl(char *s)
-{
-	ft_write_fd(s);
+	write(STDERR_FILENO, s1, ft_strlen(s1));
+	write(STDERR_FILENO, s2, ft_strlen(s2));
 	write(STDERR_FILENO, "\n", 1);
 }
 
@@ -61,12 +43,9 @@ int	main(int argc, char **argv, char **envp)
 			while (argv[i] && argv[i][0] != ';')
 				i++;
 			if (i - cmd != 2)
-				ft_write_fd("error: cd: bad arguments\n");
+				ft_write("error: cd: bad arguments", "");
 			else if (chdir(argv[cmd + 1]) == -1)
-			{
-				ft_write_fd("error: cd: cannot change directory to ");
-				ft_write_fd_nl(argv[cmd + 1]);
-			}
+				ft_write("error: cd: cannot change directory to ", argv[cmd + 1]);
 		}
 		else
 		{
@@ -77,7 +56,7 @@ int	main(int argc, char **argv, char **envp)
 				if (argv[i][0] == '|')
 				{
 					if (pipe(fd_out) == -1)
-						ft_write_fd("error: fatal\n");
+						ft_write("error: fatal", "");
 					break;
 				}
 				else if (argv[i][0]  == ';')
@@ -101,14 +80,11 @@ int	main(int argc, char **argv, char **envp)
 				}
 				argv[i] = NULL;
 				if (execve(argv[cmd], &argv[cmd], envp) == -1)
-				{
-					ft_write_fd("error: cannot execute ");
-					ft_write_fd_nl(argv[cmd]);
-				}
+					ft_write("error: cannot execute ", argv[cmd]);
 				exit(0);
 			}
 			else if (pid == -1)
-				ft_write_fd("error: fatal\n");
+				ft_write("error: fatal", "");
 			ft_close(&fd_in[0]);
 			ft_close(&fd_in[1]);
 			fd_in[0] = fd_out[0];
